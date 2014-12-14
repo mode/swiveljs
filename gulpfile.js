a385b4bc5f50e63e7ccc8a4a6139f26ddb90600d
Josh Ferguson <josh@modeanalytics.com>
2014-12-14 14:19:54 -0800

    more development environment tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,22 @@ var concat = require('gulp-continuous-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var filesize = require('gulp-filesize');
+var connect = require('gulp-connect');
 
 var distDir = 'dist';
 
-gulp.task('clean', function () {
-  return gulp.src(distDir, {read: false})
-  .pipe(clean());
+gulp.task('connect', function() {
+  return connect.server({
+    port: 9095,
+    livereload: true
+  });
 });
 
 gulp.task('build', function() {
   return gulp.src('src/**/*.js')
     .pipe(gulp.dest(distDir))
-    .pipe(filesize());
-});
-
-gulp.task('build:watch', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(watch('src/**/*.js'))
-    .pipe(concat('swivel.all.js'))
-    .pipe(gulp.dest(distDir))
-    .pipe(filesize());
+    .pipe(filesize())
+    .pipe(connect.reload());
 });
 
 gulp.task('test', function () {
@@ -33,4 +29,17 @@ gulp.task('test', function () {
     .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('default', ['build:watch']);
+gulp.task('build:watch', function() {
+  return gulp.watch(['src/**/*.js'], ['build']);
+});
+
+gulp.task('test:watch', function () {
+  return gulp.watch(['src/**/*.js, test/**/*.js'], ['test']);
+});
+
+gulp.task('clean', function () {
+  return gulp.src(distDir, {read: false})
+  .pipe(clean());
+});
+
+gulp.task('default', ['connect', 'build:watch']);
