9e294c6a1d69ebefe55f180958c521d10cad95be
Josh Ferguson <josh@modeanalytics.com>
2014-12-14 14:04:53 -0800

    bunch of build steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var clean = require('gulp-clean');
+var mocha = require('gulp-mocha');
+var concat = require('gulp-continuous-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var filesize = require('gulp-filesize');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var distDir = 'dist';
+
+gulp.task('clean', function () {
+  return gulp.src(distDir, {read: false})
+  .pipe(clean());
 });
+
+gulp.task('build', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(gulp.dest(distDir))
+    .pipe(filesize());
+});
+
+gulp.task('build:watch', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(watch('src/**/*.js'))
+    .pipe(concat('swivel.all.js'))
+    .pipe(gulp.dest(distDir))
+    .pipe(filesize());
+});
+
+gulp.task('test', function () {
+  return gulp.src('test/*.js', {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('default', ['build:watch']);
